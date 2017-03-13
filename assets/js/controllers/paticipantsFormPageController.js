a390dc9360dfeb4a84357aa3b77e4d00ae605046
maurice_hayward <mauricehayward1@gmail.com>
2017-03-13 18:13:38 -0400

    partiall working dynamic form

--- assets/js/controllers/paticipantsFormPageController.js
+++ assets/js/controllers/paticipantsFormPageController.js
@@ -1,9 +1,42 @@
-$(document).ready(function(){
-			$("#topNavContent").load("/assets/templates/partial/home_top_nav.html"); 
-    		$("#bottomNavContent").load("/assets/templates/partial/home_bottom_nav.html"); 
-			
+$(document).ready(function() {
+    $("#topNavContent").load("/assets/templates/partial/home_top_nav.html");
+    $("#bottomNavContent").load("/assets/templates/partial/home_bottom_nav.html");
 
-            $('[data-toggle="datepicker"]').datepicker();
+    $('[data-toggle="datepicker"]').datepicker();
+    $('.timepicker').wickedpicker({twentyFour: true});
 
-            $('.timepicker').wickedpicker({twentyFour: true});
-	});
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID 
+    var $template       = $("#dateTemplate");
+    
+
+    var numDateFields = 1;
+    
+     $(".addButton").click(function(e){ //on add input button click
+        e.preventDefault();
+        console.log("c")
+        var removeButton = $('<div class="col-xs-2">'
+            + '<button type="button" class="btn btn-danger remove">'
+            + '<i class="fa fa-minus">Remove This</i></button></div>');
+        removeButton.click(function() {
+            $(this).parent().remove();
+        });
+        
+        var $clone          = $template
+                                .clone()
+                                .removeClass('hide')
+                                .removeAttr('id')
+                                .append(removeButton)
+                                .insertBefore($template);
+       
+        numDateFields++; //text box increment
+        //$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        
+    });
+
+    
+    $("form").submit(function(event) {
+        console.log($(this).serializeArray());
+        event.preventDefault();
+    });
+});

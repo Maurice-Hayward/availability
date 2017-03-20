487a6816552a5014b5a6d39d7773110b23a1cac9
maurice_hayward <mauricehayward1@gmail.com>
2017-03-20 01:21:03 -0400

    use event delegation, add fonts, and add requried to form

--- assets/js/controllers/paticipantsFormPageController.js
+++ assets/js/controllers/paticipantsFormPageController.js
@@ -2,40 +2,65 @@ $(document).ready(function() {
     $("#topNavContent").load("/assets/templates/partial/home_top_nav.html");
     $("#bottomNavContent").load("/assets/templates/partial/home_bottom_nav.html");
 
-    $('[data-toggle="datepicker"]').datepicker();
-    $('.timepicker').wickedpicker({twentyFour: true, now:'00:00'});
 
-    var $template       = $("#dateTemplate");  // form Date Field template
+    const timepickerOptions = {
+        twentyFour: true,
+        now: '00:00',
+        minutesInterval: 5
+    };
     
-
+    var $template = $("#dateTemplate"); // form Date Field template
     var numDateFields = 1;
-    
-     $(".addButton").on("click", function(e){ //on add input button click
+
+    $(".addButton").on("click", function(e) { //on add input button click
         e.preventDefault();
 
+        //clones template then adds clone to the form
         var $newFormLine = $template
-                                .clone(true)
-                                .removeClass('hide')
-                                .removeAttr('id')
-                                .insertBefore($template)
+            .clone()
+            .removeClass('hide')
+            .removeAttr('id')
+            .insertBefore($template);
 
 
-        //$('[data-toggle="datepicker"]').datepicker();
-        //$('.timepicker').wickedpicker({twentyFour: true,now:'00:00'});
-       
-        numDateFields++; 
-        
+        //add "required" property to date feild
+        $newFormLine.find("input[name=date]").prop('required', true);
+
+
+        numDateFields++;
+
     });
 
-    $(".removeButton").on("click", function(e){
-         e.preventDefault();
-         $(this).parent().parent().remove();
-         numDateFields--;
-    })
+    //Use event delegation to remove form line when trash button is clicked
+    $("form").on("click", ".removeButton", function(e) {
+        e.preventDefault();
+        $(this).parent().parent().remove();
+        numDateFields--;
+    });
 
-    
-    $("form").on("submit", function(event) {
-        console.log($(this).serializeArray());
-        event.preventDefault();
+    //Use event delegation to intialized datepicker
+    $("form").on("mouseover", '[data-toggle="datepicker"]', function(e) {
+        e.preventDefault();
+
+        //make sure input feild hasn't already been intialized 
+        if (!$(this).data('intialized')) {
+            $(this).datepicker();
+            $(this).data('intialized', true);
+        }
+    });
+
+    //make sure input feild hasn't already been intialized 
+    $("form").on("mouseover", '.timepicker', function(e) {
+        e.preventDefault();
+        if (!$(this).data('intialized')) {
+            $(this).wickedpicker(timepickerOptions);
+            $(this).data('intialized', true);
+        }
+    });
+
+
+    $("form").on("submit", function(e) {
+        console.log($(this).serialize());
+        e.preventDefault();
     });
 });
